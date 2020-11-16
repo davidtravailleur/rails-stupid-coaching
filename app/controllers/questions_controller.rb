7fb4bd7906a18000c68e8e66eafaee78fbfd288a
david travailleur <travailleur.david@gmail.com>
2020-11-16 14:43:40 +0100

    exo 2 update view ask

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,19 @@
+
 class QuestionsController < ApplicationController
+
   def ask
+  end
+
+  def answer
+    @ask = params[:ask]
+    # si on a la phrase "I am going to work" => retourne "Great!" de la part du coach
+    # @answer
+    if @ask == 'I am going to work'
+      @answer = 'Great!'
+    elsif @ask.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
